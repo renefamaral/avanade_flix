9f9ee7348840474341969467be8575fb4823d998
Rene Amaral <renefamaral@gmail.com>
2021-04-09 11:31:46 -0300

    Final aulas 8 e 9. Inclusão de ação botões assistir agora e mais informações. Criação de nova página series e desenvolvimento da mesma. Necessário ajustes na nova página.

---
+++ js/owl/main.js
@@ -0,0 +1,23 @@
+function play_now_movie(){
+    window.open( "https://www.youtube.com/watch?v=mjKEXxO2KNE" ); /** função que abre o trailer oficial do filme. */ 
+}
+
+function more_information_movie(){
+    window.open("https://www.themoviedb.org/movie/363676-sully?language=pt-BR"); /** Recireciona ao TMDB */
+}
+
+function play_now_series(){
+    window.open("https://www.youtube.com/watch?v=pFmBlu--m-Y");
+}
+
+function more_information_series(){
+    window.open("https://www.themoviedb.org/tv/39259-pan-am?language=pt-BR");
+}
+
+function series(){
+    window.location.href = "http://127.0.0.1:5500/index_series.html#";
+}
+
+function home(){
+    window.location.href = "http://127.0.0.1:5500/index.html#";
+}
